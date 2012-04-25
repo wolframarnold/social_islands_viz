8c09783eec1624409b6bfa60bf72a55d94bdbf64
Weidong Yang <wdyangmail@yahoo.com>
2012-04-25 10:35:35 -0700

    ScoringApp has lower bound for clustering coefficient

--- src/main/java/com/socialislands/viz/StandAlone.java
+++ src/main/java/com/socialislands/viz/StandAlone.java
@@ -13,8 +13,8 @@ public class StandAlone {
             
 //        VizApp vizapp = new VizApp("4f63c7633f033175fe000007"); //Weidong
 //        VizApp vizapp = new VizApp("4f567b699881b47c29000001");  //Wolf
-            ScoringApp scoringapp = new ScoringApp("4f63c6e23f033175fe000004"); //Wolf
-//        ScoringApp scoringapp = new ScoringApp("4f63c7633f033175fe000007"); //Weidong
+//            ScoringApp scoringapp = new ScoringApp("4f63c6e23f033175fe000004"); //Wolf
+        ScoringApp scoringapp = new ScoringApp("4f63c7633f033175fe000007"); //Weidong
           
         }
 }
