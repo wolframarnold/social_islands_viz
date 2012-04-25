87f7cfc06709d713aabfd14eadb4906617bebdc8
Weidong Yang <wdyangmail@yahoo.com>
2012-04-25 10:36:07 -0700

    ScoringApp has lower bound for clustering coefficient

--- src/main/java/com/socialislands/viz/ScoringApp.java
+++ src/main/java/com/socialislands/viz/ScoringApp.java
@@ -4,6 +4,8 @@ import com.mongodb.*;
 import java.net.UnknownHostException;
 import java.util.Hashtable;
 import java.util.Iterator;
+import java.util.Arrays;
+
 import org.gephi.data.attributes.api.AttributeController;
 import org.gephi.data.attributes.api.AttributeModel;
 import org.gephi.graph.api.GraphView;
@@ -174,6 +176,11 @@ public class ScoringApp extends App
         System.out.println("Start export scores to MongoDB...");
         exportToMongo();
         System.out.println("Done...");
+        
+//        MeanClusteringCoefficientTable mCCTable= new MeanClusteringCoefficientTable();
+        double xi= 20;
+        System.out.println(xi + " interp to " + MeanClusteringCoefficientTable.interp(xi));
+        System.out.println(xi + " interp to " + LowerClusteringCoefficientTable.interp(xi));
     }
     
     /**
@@ -186,4 +193,117 @@ public class ScoringApp extends App
     }
 
     
+}
+
+class MyTable{
+    
+    public MyTable(){   
+    }
+    
+    public static double[] interpLinear(double[] x, double[] y, double[] xi) throws IllegalArgumentException {
+
+        if (x.length != y.length) {
+            throw new IllegalArgumentException("X and Y must be the same length");
+        }
+        if (x.length == 1) {
+            throw new IllegalArgumentException("X must contain more than one value");
+        }
+        double[] dx = new double[x.length - 1];
+        double[] dy = new double[x.length - 1];
+        double[] slope = new double[x.length - 1];
+        double[] intercept = new double[x.length - 1];
+
+        // Calculate the line equation (i.e. slope and intercept) between each point
+        for (int i = 0; i < x.length - 1; i++) {
+            dx[i] = x[i + 1] - x[i];
+            if (dx[i] == 0) {
+                throw new IllegalArgumentException("X must be montotonic. A duplicate " + "x-value was found");
+            }
+            if (dx[i] < 0) {
+                throw new IllegalArgumentException("X must be sorted");
+            }
+            dy[i] = y[i + 1] - y[i];
+            slope[i] = dy[i] / dx[i];
+            intercept[i] = y[i] - x[i] * slope[i];
+        }
+
+        // Perform the interpolation here
+        double[] yi = new double[xi.length];
+        for (int i = 0; i < xi.length; i++) {
+            if ((xi[i] > x[x.length - 1]) || (xi[i] < x[0])) {
+                yi[i] = Double.NaN;
+            }
+            else {
+                int loc = Arrays.binarySearch(x, xi[i]);
+                if (loc < -1) {
+                    loc = -loc - 2;
+                    yi[i] = slope[loc] * xi[i] + intercept[loc];
+                }
+                else {
+                    yi[i] = y[loc];
+                }
+            }
+        }
+
+        return yi;
+    }
+
+}
+
+class MeanClusteringCoefficientTable extends MyTable{
+
+    static double X[]={117,185,245,282,317,346,374,395,430,461,476,496,512,528,547,568,572,574,576};
+    static double Y[]={94,117,142,162,181,195,206,211,219,229,239,256,273,292,319,346,356,378,401};
+    static double degree[]={1.950672,6.239871,17.408297,32.774119,59.628388,97.907746,158.035512,226.312174,411.746543,699.594894,904.152721,1272.824941,1673.363082,2199.944324,3044.473716,4359.788851,4668.429805,4830.849653,4998.920267};
+    static double CC[]={0.51763945,0.38971174,0.28624589,0.223633,0.17688568,0.14881623,0.12992372,0.12214835,0.11066397,0.0978148,0.08645754,0.07009387,0.05682732,0.04494837,0.0322099,0.02308154,0.02040155,0.01555032,0.01170727};
+    static double logDegree[]={0.6681739,1.8309596,2.8569469,3.4896391,4.0881318,4.5840257,5.0628198,5.4219153,6.020408,6.5505014,6.8069983,7.1489941,7.4225907,7.6961873,8.0210833,8.3801789,8.4485781,8.4827776,8.5169772};
+    static double logCC[]={-0.6584763,-0.9423479,-1.2509041,-1.497749,-1.7322516,-1.9050431,-2.0408078,-2.102519,-2.2012569,-2.3246794,-2.4481018,-2.65792,-2.8677382,-3.1022408,-3.4354814,-3.7687221,-3.8921445,-4.1636739,-4.4475455};
+
+    public static double interp(double x){
+        double[] xlist = new double[1];
+        xlist[0] = Math.log(x);
+        double[] val = interpLinear(logDegree, logCC, xlist);
+        return Math.exp(val[0]);
+    }
+    public MeanClusteringCoefficientTable(){
+    }
+}
+
+class LowerClusteringCoefficientTable extends MyTable{
+
+    static double X[]={210,251,305,348,387,440,463,480,502,515};
+    static double Y[]={306,287,288,297,308,330,346,366,394,421};
+    static double degree[]={9.568288,19.289206,48.566515,101.314065,197.377281,488.532436,723.934575,968.160077,1410.349476,1761.445522};
+    static double CC[]={0.03781565,0.047809563,0.047223112,0.042258403,0.036893616,0.02812079,0.023081541,0.018032728,0.012763703,0.009146441};
+    static double logDegree[]={2.258454,2.959546,3.882934,4.618225,5.285117,6.191406,6.584701,6.875397,7.251593,7.47389};
+    static double logCC[]={-3.275032,-3.04053,-3.052872,-3.163952,-3.299717,-3.571246,-3.768722,-4.015567,-4.36115,-4.69439};
+
+    public static double interp(double x){
+        double[] xlist = new double[1];
+        xlist[0] = Math.log(x);
+        double[] val = interpLinear(logDegree, logCC, xlist);
+        return Math.exp(val[0]);
+    }
+    public LowerClusteringCoefficientTable(){
+    }
+}
+
+class UpperClusteringCoefficientTable extends MyTable{
+
+    
+    static double X[]={};
+    static double Y[]={};
+    static double degree[]={};
+    static double CC[]={};
+    static double logDegree[]={};
+    static double logCC[]={};
+
+    public static double interp(double x){
+        double[] xlist = new double[1];
+        xlist[0] = Math.log(x);
+        double[] val = interpLinear(logDegree, logCC, xlist);
+        return Math.exp(val[0]);
+    }
+    public UpperClusteringCoefficientTable(){
+    }
 }
